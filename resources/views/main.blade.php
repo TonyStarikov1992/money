3981b8313cacce2fe65f6dff2c9e568c59f8af29
anton_starikov <tony.starikov.1992@gmail.com>
2021-06-23 00:03:41 +0300

    Edit chart

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,24 +12,24 @@
             <div class="col-md-6 mt-2">
                 <!-- TradingView Widget BEGIN -->
                 <div class="tradingview-widget-container">
-                    <div id="tradingview_8b1d4"></div>
-                    <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/AAVEBTC/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">AAVEBTC Chart</span></a> by TradingView</div>
+                    <div id="tradingview_1447d"></div>
+                    <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com/" rel="noopener" target="_blank"><span class="blue-text">График USD/EU</span></a> от TradingView</div>
                     <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
                     <script type="text/javascript">
                         new TradingView.widget(
                             {
                                 "width": 980,
                                 "height": 610,
-                                "symbol": "BINANCE:AAVEBTC",
+                                "symbol": "USD/EU",
                                 "interval": "D",
                                 "timezone": "Etc/UTC",
                                 "theme": "light",
                                 "style": "1",
-                                "locale": "uk",
+                                "locale": "ru",
                                 "toolbar_bg": "#f1f3f6",
                                 "enable_publishing": false,
                                 "allow_symbol_change": true,
-                                "container_id": "tradingview_8b1d4"
+                                "container_id": "tradingview_1447d"
                             }
                         );
                     </script>
