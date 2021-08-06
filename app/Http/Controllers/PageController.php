03612b81b6bbf482b6d3f61fa536eeb0fb0a3f23
anton_starikov <tony.starikov.1992@gmail.com>
2021-08-06 19:42:02 +0300

    Trading test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
@@ -23,6 +24,20 @@ class PageController extends Controller
 
     public function analytics()
     {
-        return view('analytics');
+        $user_money = Auth::user()->check;
+        $random_percent = random_int(1, 2);
+        $random_sign = random_int(1, 20);
+        $bonus = ($user_money/100) * $random_percent;
+        if ($random_sign > 15) {
+            $user_money += -$bonus;
+            $random_sign = '-';
+        } else {
+            $user_money += $bonus;
+            $random_sign = '+';
+        }
+        Auth::user()->check = $user_money;
+        Auth::user()->save();
+
+        return view('analytics' , compact('user_money', 'random_percent', 'bonus', 'random_sign'));
     }
 }
--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -5,7 +5,17 @@
 @section('main')
     <div class="container-fluid">
         <div class="row">
-            <h1>Analytics</h1>
+            <div class="col">
+                <h1>Analytics</h1>
+                <div class="vh-100">
+                    <p>Money: {{ $user_money }} $</p>
+                    <p>Random %: {{ $random_percent }} %</p>
+                    <p>Random +/-: {{ $random_sign }}</p>
+                    <p>Bonus: {{ $bonus }} $</p>
+                    <p>Start</p>
+                </div>
+            </div>
+
         </div>
     </div>
     <!-- /container -->
