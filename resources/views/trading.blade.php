243e60cf33275b32f3dc275360fcc19b5d80b84e
TonyStarikov1992 <tony.starikov.1992@gmail.com>
2021-12-20 20:58:50 +0200

    Edit chart on trading page

--- resources/views/trading.blade.php
+++ resources/views/trading.blade.php
@@ -25,23 +25,43 @@
 
                     <!-- TradingView Widget BEGIN -->
                     <div class="tradingview-widget-container" style="height: 600px">
-                        <div class="h-100" id="tradingview_6c5b1"></div>
-                        <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com/symbols/BTCUSD/?exchange=COINBASE" rel="noopener" target="_blank"><span class="blue-text">График BTCUSD</span></a> от TradingView</div>
+                        <div class="h-100" id="watchlist"></div>
+                        <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">График AAPL</span></a> от TradingView</div>
                         <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
                         <script type="text/javascript">
                             new TradingView.widget(
                                 {
+                                    "container_id": "watchlist",
                                     "autosize": true,
                                     "symbol": "COINBASE:BTCUSD",
-                                    "interval": "D",
-                                    "timezone": "Etc/UTC",
+                                    "interval": "1",
+                                    "timezone": "exchange",
                                     "theme": "light",
                                     "style": "1",
-                                    "locale": "ru",
                                     "toolbar_bg": "#f1f3f6",
-                                    "enable_publishing": false,
+                                    "withdateranges": true,
                                     "allow_symbol_change": true,
-                                    "container_id": "tradingview_6c5b1"
+                                    "save_image": false,
+                                    "watchlist": [
+                                        "COINBASE:BTCUSD",
+                                        "COINBASE:SHIBUSDT",
+                                        "COINBASE:ETHUSD",
+                                        "COINBASE:DOGEUSD",
+                                        "BITFINEX:XRPUSD",
+                                        "FTX:MATICUSD",
+                                        "COINBASE:ADAUSD",
+                                        "BINANCEUS:SOLUSD",
+                                        "BINANCE:DATAUSD",
+                                        "BINANCE:BNBUSD",
+                                        "COINBASE:XLMUSD",
+                                        "BINANCE:TRXUSD",
+                                        "FTX:HTUSD",
+                                        "BINANCE:DOTUSD",
+                                        "COINBASE:LINKUSD",
+                                        "COINBASE:BCHUSD",
+                                        "COINBASE:LTCUSD",
+                                    ],
+                                    "locale": "ru"
                                 }
                             );
                         </script>
@@ -72,23 +92,43 @@
 
                     <!-- TradingView Widget BEGIN -->
                     <div class="tradingview-widget-container" style="height: 600px">
-                        <div class="h-100" id="tradingview_6c5b1"></div>
-                        <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com/symbols/BTCUSD/?exchange=COINBASE" rel="noopener" target="_blank"><span class="blue-text">График BTCUSD</span></a> от TradingView</div>
+                        <div class="h-100" id="watchlist"></div>
+                        <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">График AAPL</span></a> от TradingView</div>
                         <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
                         <script type="text/javascript">
                             new TradingView.widget(
                                 {
+                                    "container_id": "watchlist",
                                     "autosize": true,
                                     "symbol": "COINBASE:BTCUSD",
-                                    "interval": "D",
-                                    "timezone": "Etc/UTC",
+                                    "interval": "1",
+                                    "timezone": "exchange",
                                     "theme": "light",
                                     "style": "1",
-                                    "locale": "ru",
                                     "toolbar_bg": "#f1f3f6",
-                                    "enable_publishing": false,
+                                    "withdateranges": true,
                                     "allow_symbol_change": true,
-                                    "container_id": "tradingview_6c5b1"
+                                    "save_image": false,
+                                    "watchlist": [
+                                        "COINBASE:BTCUSD",
+                                        "COINBASE:SHIBUSDT",
+                                        "COINBASE:ETHUSD",
+                                        "COINBASE:DOGEUSD",
+                                        "BITFINEX:XRPUSD",
+                                        "FTX:MATICUSD",
+                                        "COINBASE:ADAUSD",
+                                        "BINANCEUS:SOLUSD",
+                                        "BINANCE:DATAUSD",
+                                        "BINANCE:BNBUSD",
+                                        "COINBASE:XLMUSD",
+                                        "BINANCE:TRXUSD",
+                                        "FTX:HTUSD",
+                                        "BINANCE:DOTUSD",
+                                        "COINBASE:LINKUSD",
+                                        "COINBASE:BCHUSD",
+                                        "COINBASE:LTCUSD",
+                                    ],
+                                    "locale": "ru"
                                 }
                             );
                         </script>
